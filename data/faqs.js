15671800f485ff95c50814ec79376e36cba86337
Chris <chris@metaspeedclub.net>
2022-10-25 12:51:40 +0100

    faqs

--- data/faqs.js
+++ data/faqs.js
@@ -2,32 +2,32 @@ const faqDatas = [
     {
         id: 1,
         title: 'What is FASC NFT?',
-        info: 'It is 1 of unique 3836 NFTs on the Ethereum blockchain that will have future utilites',
+        info: 'It is 1 of unique 3836 NFTs on the Solana blockchain that will have future utilites',
     },
     {
         id: 2,
-        title: 'Can I buy an NFT from Opensea?',
-        info: 'Yes if you missed the minting the NFTs can be bought on secondary marketplaces such as Opensea',
+        title: 'Can I buy an NFT from Magic Eden?',
+        info: 'Yes if you missed the minting the NFTs can be bought on secondary marketplaces such as Magic Eden',
     },
     {
         id: 3,
         title: 'How Can I Use My NFT ?',
-        info: 'We are working on giving utitlites to the NFT holders, this will be announced at a later date',
+        info: 'We are working on giving utitlites to the NFT holders, such as alpha calls and staking to start',
     },
     {
         id: 4,
         title: 'What type of wallet do i need to buy a NFTs',
-        info: 'One of the most common wallet that is used is MetaMask and you will be able to mint using that wallet',
+        info: 'One of the most common wallet that is used on Solana is Phantom and you will be able to mint using that wallet',
     },
     {
         id: 5,
         title: 'Where can I View My NFTS ?',
-        info: 'You can see your NFTs by using secondary marketplace such as Opensea within your Profile or in your wallet if it allows this (Metamask - Mobile)',
+        info: 'You can see your NFTs by using secondary marketplace such as Magic Eden within your Profile',
     },
     {
         id: 6,
         title: 'Where we can buy and sell NFTs?',
-        info: 'After minting the NFTs can be bought and sold on secondary marketplaces such as opensea and GEM',
+        info: 'After minting the NFTs can be bought and sold on secondary marketplaces such as Magic Eden',
     },
 ];
 
